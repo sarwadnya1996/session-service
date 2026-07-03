32c6e66f0d37a4a925cf8caf2079c12a20aed2cd
sarwadnya <sarwadnya1996@gmail.com>
2026-07-03 22:01:48 +0530

    initial commit

---
+++ session-service/src/main/java/com/example/session_service/SessionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.session_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionServiceApplication.class, args);
+	}
+
+}
